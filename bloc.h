ba5da1863c207bb59192ca8a75e84cc2e78cbf0b
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-09-02 14:18:48 +0900

    端の4分の1は落下判定 カメラはblocについていく リセットも入っている

--- bloc.h
+++ bloc.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "KamataEngine.h"
-#include <vector>
 #include <memory>
+#include <vector>
 using namespace KamataEngine;
 
 class bloc {
@@ -10,7 +10,7 @@ public:
 	/// 初期化
 	void Initialize(Model* model, Camera* camera);
 
-	//動き
+	// 動き
 	void Move();
 
 	/// 更新
@@ -28,10 +28,26 @@ private:
 	WorldTransform worldTransform_;
 	// 積み上げたブロック
 	std::vector<std::unique_ptr<WorldTransform>> blocks_;
-	
 
 	// 移動方向
 	float moveDirection_ = 0.2f;
 	// 停止しているか
 	bool isStop_ = false;
+
+	// ブロックの横幅
+	float blockWidth_ = 4.0f;
+
+	// 落下中か
+	bool isFalling_ = false;
+
+	// 落下速度
+	float fallSpeed_ = 0.2f;
+
+
+	// カメラの初期位置
+	Vector3 cameraStartPosition_;
+	//カメラ
+	float cameraOffsetY_ = 0.0f;
+	float cameraMoveY_ = 2.0f;
+
 };
